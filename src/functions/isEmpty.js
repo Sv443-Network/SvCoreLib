ff7e7fc8bc29032a0b69bebc42ae0666d6f4a092
Sven Fehler <sven.fehler@web.de>
2021-02-02 08:07:58 +0100

    fixed LGTM alerts

--- src/functions/isEmpty.js
+++ src/functions/isEmpty.js
@@ -2,8 +2,8 @@ function isEmpty(input)
 {
     return (
            (input === undefined || input === null || input === "") // other
-        || (input != null && Array.isArray(input) && input.length == 0) // arrays
-        || (input !== null && typeof input == "object" && !Array.isArray(input) && Object.keys(input).length == 0) // objects
+        || (typeof input === "object" && Array.isArray(input) && input.length === 0) // arrays
+        || (typeof input === "object" && !Array.isArray(input) && Object.keys(input) && Object.keys(input).length == 0) // objects
     ) ? true : false;
 }
 
